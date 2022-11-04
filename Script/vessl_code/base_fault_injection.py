ed0d9241577533767aebfd875370adf18f31b176
WaiNaat <qoli0703@gmail.com>
2022-11-04 15:57:54 +0900

    Update base_fault_injection.py

--- Script/vessl_code/base_fault_injection.py
+++ Script/vessl_code/base_fault_injection.py
@@ -5,21 +5,6 @@ from pytorchfi.core import FaultInjection
 from bitstring import BitArray
 from collections import deque
 
-class add_input_layer(torch.nn.Module):
-    '''
-    You can use this class when you want fault injection to input tensor itself.    
-    '''
-    def __init__(self, model, *args):
-        super().__init__(*args)
-        self.input_layer = torch.nn.Identity()
-        self.model = model
-
-    def forward(self, x):
-        input = self.input_layer(x)
-        output = self.model(input)
-        return output
-
-
 class single_bit_flip_model(FaultInjection):
     def __init__(self, model, batch_size, flip_bit_pos=None, save_log_list=False, flip_bit_pos_list=[], **kwargs):
         super().__init__(model, batch_size, **kwargs)
