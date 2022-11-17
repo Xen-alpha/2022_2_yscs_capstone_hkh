393a26d5520618984f94cb528e58f9ebc37b98df
Q Kim <qoli0703@gmail.com>
2022-11-17 15:59:14 +0900

    Update get_block_max_value.py

--- Script/vessl_code/get_block_max_value.py
+++ Script/vessl_code/get_block_max_value.py
@@ -59,4 +59,6 @@ if __name__ == '__main__':
       result = model(images)
 
     for i, value in enumerate(tool.result):
-      vessl.log(step=batch_idx, payload={f'block {i}': value})
+      vessl.log(step=batch_idx, payload={f'block {i}': value.item()})
+
+  print(tool.result)
